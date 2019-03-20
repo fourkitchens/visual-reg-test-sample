93ff8f6ff1b423e115853fabb5eb14244e90adce
Adam Erickson <adam@fourkitchens.com>
2019-03-19 20:41:24 -0500

    adding tugboat

---
+++ .tugboat/settings.local.php
@@ -0,0 +1,16 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'tugboat',
+  'username' => 'tugboat',
+  'password' => 'tugboat',
+  'prefix' => '',
+  'host' => 'mysql',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+// Add config split folder 'dev'.
+$config['config_split.config_split.dev']['status'] = TRUE;
+
+ini_set('memory_limit', '256M');
